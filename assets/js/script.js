08ec0ad6fa345bd14668d2173728416997e4e8f3
Ronan-O-Murchu <ronan.murphy.design@gmail.com>
2021-08-07 11:56:54 +1200

    script:
    begin work on scripts.

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function(){
+    console.log('DOM fully loaded');
+    let document.getElementById('play-iamge');
+    for ('play-iamge') addEventListener("click", function() {
+    	loadGame ()
+    }
+});
+
+
+	function loadGame () {
+
+	}
+	
+
+	function makeSelection () {
+
+	}
+
+
+	function startTimer () {
+
+	}
+
+
+	function checkResult () {
+
+	}
+
+	
+	function updateScore () {
+
+	}
