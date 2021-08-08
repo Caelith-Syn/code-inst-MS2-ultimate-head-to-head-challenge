6d5e0ba17783a001b1b972a59448cf190ff3069c
Ronan-O-Murchu <ronan.murphy.design@gmail.com>
2021-08-08 15:32:57 +1200

    script:
    update cpu random choice to be displayed on right hand side of screen when a player makes a choice.

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,35 +22,51 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-	// function loadGame () {
-
-	// }
-	
-
-	function cpuSelection () {
-		let choosenIconCpu = Math.random() *2;
-		if (cpuSelection === 0) {
-			return 'rockChoosenIconCpu'
-		} if (cpuSelection === 1) {
-			return 'paperChoosenIconCpu'
-		} if (cpuSelection === 2) {
-			return 'scissorsChoosenIconCpu'
-		} else {return 'waiting for selection'}
-	}
 
 
-	function startTimer() {
-	var timeleft = 60;
+
+function loadGame () {
+
+}
+
+
+
+
+
+
+function startTimer() {
+	var timeleft = 30;
 	var downloadTimer = setInterval(function(){
-	  if(timeleft <= 0){
-	    clearInterval(downloadTimer);
-	    document.getElementById("countdown-timer").innerHTML = "Times Up";
-	  } else {
-	    document.getElementById("countdown-timer").innerHTML = timeleft;
-	  }
-	  timeleft -= 1;
-	}, 1000);
+	if(timeleft <= 0){
+	clearInterval(downloadTimer);
+	document.getElementById("countdown-timer").innerHTML = "Times Up";
+	} else {
+	document.getElementById("countdown-timer").innerHTML = timeleft;
 	}
+	timeleft -= 1;
+	}, 1000);
+}
+
+
+
+
+
+/** This code compares the players selection and the computers selection * */
+function checkAnswer() {let userChoice = document.getElementById("rockChoosenIconPlayer");
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+    if ('userChoice' === 'cpuChoice') {
+	alert("It's a draw.");
+	incrementPlayerScore();
+    } else if ('userChoice' !== 'cpuChoice') {
+   	alert("You Won!");
+   	incrementPlayerScore();
+    } else {
+   	alert(`Awwww.... you lost`);
+   	incrementCpuScore();
+    }
+
+}
 
 
 
@@ -58,32 +74,75 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
+/** This code displays the computers selection on the right hand side of the screen * */
+function showCpuChoiceRock() {
+   	document.getElementById('rockChoosenIconCpu').style.display = "block";
+	hideCpuChoicePaper();
+	hideCpuChoiceScissors();
+}
+
+function showCpuChoicePaper() {
+   	document.getElementById('paperChoosenIconCpu').style.display = "block";
+	hideCpuChoiceRock();
+	hideCpuChoiceScissors();
+}
+
+function showCpuChoiceScissors() {
+   	document.getElementById('scissorsChoosenIconCpu').style.display = "block";
+	hideCpuChoiceRock();
+	hideCpuChoicePaper();
+}
 
 
 
 
 
+/** This code generates a random selcetion for the computer to display * */
+function cpuSelection() {
+    let cpuChoice = Math.floor(Math.random()*3);
+    if (cpuChoice === 1) {
+    return showCpuChoiceRock()
+    } else if (cpuChoice === 2) {
+   	return showCpuChoicePaper()
+    } else if (cpuChoice === 3) {
+    return  showCpuChoiceScissors()
+    } else {
+    return "Make a selection to begin the game";
+    }
+}
 
 
-	function showPlayerChoiceRock() {
+
+
+
+/** This code displays the players selection on the left hand side of the screen * */
+function showPlayerChoiceRock() {
    	document.getElementById('rockChoosenIconPlayer').style.display = "block";
 	hidePlayerChoicePaper();
 	hidePlayerChoiceScissors();
+	cpuSelection();
 	}
 
-	function showPlayerChoicePaper() {
+function showPlayerChoicePaper() {
    	document.getElementById('paperChoosenIconPlayer').style.display = "block";
 	hidePlayerChoiceRock();
 	hidePlayerChoiceScissors();
-	}
+	cpuSelection();
+} 
 
-	function showPlayerChoiceScissors() {
+function showPlayerChoiceScissors() {
    	document.getElementById('scissorsChoosenIconPlayer').style.display = "block";
 	hidePlayerChoiceRock();
 	hidePlayerChoicePaper();
-	}
+	cpuSelection ();
+}
 
 
+
+
+
+
+/** This code hides the players selection on the left hand side of the screen * */
 function hidePlayerChoiceRock() {
    document.getElementById('rockChoosenIconPlayer').style.display = "none";
 }
@@ -95,3 +154,69 @@ function hidePlayerChoicePaper() {
 function hidePlayerChoiceScissors() {
    document.getElementById('scissorsChoosenIconPlayer').style.display = "none";
 }
+
+
+
+
+
+
+/** This code hides the computers selection on the right hand side of the screen * */
+function hideCpuChoiceRock() {
+   document.getElementById('rockChoosenIconCpu').style.display = "none";
+}
+
+function hideCpuChoicePaper() {
+   document.getElementById('paperChoosenIconCpu').style.display = "none";
+}
+
+function hideCpuChoiceScissors() {
+   document.getElementById('scissorsChoosenIconCpu').style.display = "none";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/** This code compares the players selection and the computers selection * */
+if (string1 === cpuSelection()) {
+  console.log("Matching strings!");
+}
+else {
+  console.log("Strings do not match");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
